028269449cd3b4df231f9b72f4f9434350319a13
dave <dpadrino@hiberus.com>
2020-09-28 13:32:27 +0200

    fix bug

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/coffee";
 } else {
-  urlDB = process.env.MONGO_URI:;
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
